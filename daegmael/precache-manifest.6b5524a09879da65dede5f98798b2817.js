d5917d99b07fa5816da3dc7a09696eadb0957e05
AbFarid <farid@MacBook.local>
2019-01-13 20:46:43 +0200

    Move DaegMael to a subfolder
    Update DaegMael

---
+++ daegmael/precache-manifest.6b5524a09879da65dede5f98798b2817.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "36d6672adcdee2573292",
+    "url": "/daegmael/css/app.bd6c5672.css"
+  },
+  {
+    "revision": "36d6672adcdee2573292",
+    "url": "/daegmael/js/app.3ca91fa0.js"
+  },
+  {
+    "revision": "ebcf0e69766bfd281262",
+    "url": "/daegmael/js/chunk-vendors.b9d16972.js"
+  },
+  {
+    "revision": "6d230dd890a17d8c2b4a4a591d95ef53",
+    "url": "/daegmael/img/sun.6d230dd8.png"
+  },
+  {
+    "revision": "17e98350707754d49f2c79d823945788",
+    "url": "/daegmael/img/ornament_gold.17e98350.png"
+  },
+  {
+    "revision": "38a4b018e343203212d467b90abaef79",
+    "url": "/daegmael/img/brick_wall_pattern.38a4b018.png"
+  },
+  {
+    "revision": "7499826db43b9c9812861d87ba6bbf0e",
+    "url": "/daegmael/fonts/archer.7499826d.ttf"
+  },
+  {
+    "revision": "1489f9418cba6993283af9f1cc56b6d1",
+    "url": "/daegmael/fonts/Stgotic.1489f941.otf"
+  },
+  {
+    "revision": "a64c326c1f8f5a2927b0fea8bd82a158",
+    "url": "/daegmael/fonts/orange_kid.a64c326c.ttf"
+  },
+  {
+    "revision": "0476c5cbc3633e1d5528d7341f73598f",
+    "url": "/daegmael/img/crown.0476c5cb.png"
+  },
+  {
+    "revision": "23c54fb5fb278ca80a17b5ca770bb465",
+    "url": "/daegmael/img/menu.23c54fb5.png"
+  },
+  {
+    "revision": "90c35a3ee6b25d7f5c314e15d8cdc2b0",
+    "url": "/daegmael/img/input.90c35a3e.png"
+  },
+  {
+    "revision": "ebdf5da6ecf84721b2fd528c138c8e77",
+    "url": "/daegmael/img/input_focus.ebdf5da6.png"
+  },
+  {
+    "revision": "80e8790c081a5cfca8c97fe4c49d9dab",
+    "url": "/daegmael/img/button.80e8790c.png"
+  },
+  {
+    "revision": "6aa1d38f1dfba25ffd1fdd4cb56494d7",
+    "url": "/daegmael/img/button_hover.6aa1d38f.png"
+  },
+  {
+    "revision": "b26d94d69884a5aa224826ada4cb1107",
+    "url": "/daegmael/img/button_active.b26d94d6.png"
+  },
+  {
+    "revision": "2577134973e6fa3dc40c893880d850aa",
+    "url": "/daegmael/img/button_disabled.25771349.png"
+  },
+  {
+    "revision": "52cf7068da60a603916de4c8d29af8cc",
+    "url": "/daegmael/img/moon.52cf7068.png"
+  },
+  {
+    "revision": "d9f179e36e11fcc5d49dee32cf208b62",
+    "url": "/daegmael/img/banner.d9f179e3.png"
+  },
+  {
+    "revision": "a686eebc2fe42cc5aefffb110434ee87",
+    "url": "/daegmael/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/daegmael/robots.txt"
+  }
+];
